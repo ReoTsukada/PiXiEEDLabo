8a93b83adaa7e71c4b27b8829e0f17c62ef9a25b
Taito3441 <taidouguxie470@gmail.com>
2025-10-26 18:29:13 +0900

    chore(capacitor): add config, iOS meta tags, manifest enhancements, and iOS CI build

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pixieedlabo.app',
+  appName: 'PiXiEEDraw',
+  webDir: '.',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
+
+
